fd4b5f55cacaa94a9f5e49ee69161fe7829dac78
ESTELA ALVES DE MORAES <23111781@CFP127ALUNO.LOCAL>
2024-05-21 17:20:48 -0300

    insert

--- nolance_insert.sql
+++ nolance_insert.sql
@@ -8,5 +8,13 @@ INSERT INTO tbl_subcategoria(nome, categoria_id) values
 ('Motos', 1),
 ('Ônibus', 1);
 
+INSERT INTO tbl_comitente(nome, telefone, email) values 
+('Postman da Silva', '967549234', 'postman@email.com'),
+('Acme Filmes', '967549234', 'acme@email.com');
 
+INSERT INTO tbl_pessoa_fisica(cpf, rg, comitente_id) values
+('99877655432', '54456789', 1);
+
+INSERT INTO tbl_pessoa_juridica(cnpj, razao_social, descricao, comitente_id) values
+('9987765543222', 'Acme Filmes S.A.','Acme filmes.......' , 2);
                  
--- nolance_select.sql
+++ nolance_select.sql
@@ -1,3 +1,9 @@
 select * from tbl_subcategoria join tbl_categoria on tbl_subcategoria.categoria_id=tbl_categoria.id where tbl_categoria.id = 1;
 select tbl_subcategoria.id, tbl_subcategoria.nome from tbl_subcategoria join tbl_categoria on tbl_subcategoria.categoria_id=tbl_categoria.id
          where tbl_categoria.id = 1;
+         
+         
+select * from tbl_comitente join tbl_pessoa_fisica on tbl_comitente.id=tbl_pessoa_fisica.comitente_id; 
+select * from tbl_comitente join tbl_pessoa_juridica on tbl_comitente.id=tbl_pessoa_juridica.comitente_id; 
+
+select * from tbl_comitente 
