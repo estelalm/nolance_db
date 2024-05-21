2a82f9b98ab237aa5711fce5d7752d3bcbe89273
ESTELA ALVES DE MORAES <23111781@CFP127ALUNO.LOCAL>
2024-05-21 15:36:13 -0300

    insert

---
+++ nolance_insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO tbl_categoria (nome, icone) values
+('Automóveis', 'automoveis.png'),
+('Eletrodomésticos', 'eletrodomesticos.png'),
+('Antiguidades', 'antiguidades.png');
+
+INSERT INTO tbl_subcategoria(nome, categoria_id) values 
+('Carros', 1),
+('Motos', 1),
+('Ônibus', 1);
+
+
+                  
